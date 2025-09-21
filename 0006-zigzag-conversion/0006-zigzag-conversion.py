4501270e321b079ee9e0c218990f032eda15e7f3
Mriganka Barman <119776446+onemriganka@users.noreply.github.com>
2025-09-21 23:55:00 +0530

    Time: 3 ms (98.89%), Space: 17.7 MB (90.12%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,20 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+        
+        # Create a list of empty strings for each row
+        rows = [''] * numRows
+        current_row, going_down = 0, False
+        
+        # Traverse the string and place characters in the appropriate row
+        for char in s:
+            rows[current_row] += char
+            if current_row == 0 or current_row == numRows - 1:
+                going_down = not going_down
+            current_row += 1 if going_down else -1
+        
+        # Concatenate all rows to get the final string
+        return ''.join(rows)
+
+        
